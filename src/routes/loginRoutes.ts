8190a46b095acb144b6e8f1e8963f7be925ee5b5
Long Do <longdo@dobtech.vn>
2022-03-12 10:27:58 +0700

    refactor code

---
+++ src/routes/loginRoutes.ts
@@ -0,0 +1,13 @@
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>
+      <h1>Hi There!</h1>
+    </div>
+  `);
+});
+
+export { router };
