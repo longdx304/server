1e85ee3d33b415791af00a3a77380307e95e3250
Long Do <longdo@dobtech.vn>
2022-03-12 23:29:00 +0700

    save login status into cookie session

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cookieSession from 'cookie-session';
 import { router } from './routes/loginRoutes';
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['asdas'] }));
 app.use(router);
 
 app.listen(3000, () => {
