6904860883b3f06d554ac647b8c63eeada24c570
Long Do <longdo@dobtech.vn>
2022-03-23 00:06:29 +0700

    refactor post request & validator to use decorator

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,5 +1,5 @@
-import { NextFunction, Request, Response } from 'express';
-import { get, controller, use } from './decorators';
+import { Request, Response } from 'express';
+import { get, controller, post, bodyValidator } from './decorators';
 
 @controller('/auth')
 class LoginController {
@@ -9,7 +9,7 @@ class LoginController {
     <form method="POST">
       <div>
         <label>Email</label>
-        <input name="email" />
+        <input name="e" />
       </div>
       <div>
         <label>Password</label>
@@ -18,5 +18,18 @@ class LoginController {
       <button>Submit</button>
     </form>
   `);
+  }
+
+  @post('/login')
+  @bodyValidator('email', 'password')
+  postLogin(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    if (email === 'hi@hi.com' && password === 'password') {
+      req.session = { loggedIn: true };
+      res.redirect('/');
+    } else {
+      res.send('Invalid email or password');
+    }
   }
 }
--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,7 +1,26 @@
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import 'reflect-metadata';
 import { AppRouter } from '../../AppRouter';
 import { MetadataKeys } from './MetadataKeys';
 import { Methods } from './Methods';
+
+function bodyValidator(keys: string): RequestHandler {
+  return function (req: Request, res: Response, next: NextFunction) {
+    if (!req.body) {
+      res.status(422).send('Invalid request');
+      return;
+    }
+
+    for (let key of keys) {
+      if (!req.body[key]) {
+        res.status(422).send(`Missing property ${key}`);
+        return;
+      }
+    }
+
+    next();
+  };
+}
 
 export function controller(routePrefix: string) {
   return function (target: Function) {
@@ -22,9 +41,19 @@ export function controller(routePrefix: string) {
       const middlewares =
         Reflect.getMetadata(MetadataKeys.Middleware, target.prototype, key) ||
         [];
+      const requiredBodyProps =
+        Reflect.getMetadata(MetadataKeys.Validator, target.prototype, key) ||
+        [];
+
+      const validator = bodyValidator(requiredBodyProps);
 
       if (path) {
-        router[method](`${routePrefix}${path}`, ...middlewares, routeHandler);
+        router[method](
+          `${routePrefix}${path}`,
+          ...middlewares,
+          validator,
+          routeHandler
+        );
       }
     }
   };
